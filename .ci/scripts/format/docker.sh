021578bee6cd3bec53762611d84d65039e04885e
Phoenix <phoenix@uzuy-edge.org>
2024-08-21 12:54:45 +1000

    chore: .ci/.github Temporarily Removed
    Signed-off-by: Phoenix <phoenix@uzuy-edge.org>

--- .ci/scripts/format/docker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -ex
-
-# SPDX-FileCopyrightText: 2019 uzuy Emulator Project
-# SPDX-License-Identifier: GPL-2.0-or-later
-
-# Run clang-format
-cd /uzuy
-chmod a+x ./.ci/scripts/format/script.sh
-./.ci/scripts/format/script.sh
