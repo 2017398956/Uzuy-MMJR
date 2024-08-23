19c2023966c25cfd029428463fbdb1eb10848bc4
Phoenix <phoenix@uzuy-edge.org>
2024-08-23 18:57:06 +1000

    Revert "video_core: Implement Workaround for AMD LogicOp Bug"
    This reverts commit 0711e16dc15b6e25fea01aad849e530ea6d547cd.

--- src/video_core/renderer_opengl/gl_rasterizer.cpp
+++ src/video_core/renderer_opengl/gl_rasterizer.cpp
@@ -1198,24 +1198,13 @@ void RasterizerOpenGL::SyncBlendState() {
 }
 
 void RasterizerOpenGL::SyncLogicOpState() {
-    if (!maxwell3d->dirty.flags[Dirty::LogicOp]) {
+    auto& flags = maxwell3d->dirty.flags;
+    if (!flags[Dirty::LogicOp]) {
         return;
     }
-    maxwell3d->dirty.flags[Dirty::LogicOp] = false;
+    flags[Dirty::LogicOp] = false;
 
-    auto& regs = maxwell3d->regs;
-
-    if (device.IsAmd()) {
-        bool has_float = std::any_of(
-            regs.vertex_attrib_format.begin(),
-            regs.vertex_attrib_format.end(),
-            [](const Tegra::Engines::Maxwell3D::Regs::VertexAttribute& attrib) {
-                return attrib.type == Tegra::Engines::Maxwell3D::Regs::VertexAttribute::Type::Float;
-            });
-
-        regs.logic_op.enable = !has_float;
-    }
-
+    const auto& regs = maxwell3d->regs;
     if (regs.logic_op.enable) {
         glEnable(GL_COLOR_LOGIC_OP);
         glLogicOp(MaxwellToGL::LogicOp(regs.logic_op.op));
