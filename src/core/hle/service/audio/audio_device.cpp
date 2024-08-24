818e2dc7c8b55a145904dff4782fada6916c7623
Phoenix <phoenix@uzuy-edge.org>
2024-08-24 12:49:04 +1000

    firmware: Added Support For Firmware 18.0.0+
    - Signed-off-by: Phoenix <phoenix@uzuy-edge.org>

--- src/core/hle/service/audio/audio_device.cpp
+++ src/core/hle/service/audio/audio_device.cpp
@@ -29,6 +29,12 @@ IAudioDevice::IAudioDevice(Core::System& system_, u64 applet_resource_user_id, u
         {12, D<&IAudioDevice::QueryAudioDeviceOutputEvent>, "QueryAudioDeviceOutputEvent"},
         {13, D<&IAudioDevice::GetActiveAudioDeviceName>, "GetActiveAudioOutputDeviceName"},
         {14, D<&IAudioDevice::ListAudioOutputDeviceName>, "ListAudioOutputDeviceName"},
+        {15, nullptr, "AcquireAudioInputDeviceNotification"}, // 17.0.0+
+        {16, nullptr, "ReleaseAudioInputDeviceNotification"}, // 17.0.0+
+        {17, nullptr, "AcquireAudioOutputDeviceNotification"}, // 17.0.0+
+        {18, nullptr, "ReleaseAudioOutputDeviceNotification"}, // 17.0.0+
+        {19, nullptr, "SetAudioDeviceOutputVolumeAutoTuneEnabled"}, // 18.0.0+
+        {20, nullptr, "IsAudioDeviceOutputVolumeAutoTuneEnabled"} // 18.0.0+
     };
     RegisterHandlers(functions);
 
